4b9b168987b0ea83a0bf7b11c2269e7e9a66c85f
abd-dahhan <a.dahhan94@gmail.com>
2026-01-12 03:50:29 +0100

    fix(auth): enable password hashing in UserSerializer to fix login 401

--- backend/igaveapp/serializers.py
+++ backend/igaveapp/serializers.py
@@ -20,10 +20,25 @@ class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
 
 
 class UserSerializer(serializers.ModelSerializer):
+    # 1. Explicitly define password so we can make it write_only
+    password = serializers.CharField(write_only=True)
+
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'first_name', 'last_name']
+        # 2. Add 'password' to the fields list
+        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'password']
         read_only_fields = ['id']
+
+    # 3. Override create to Hash the password!
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data.get('email', ''),
+            password=validated_data['password'], # <--- Hashing happens inside create_user
+            first_name=validated_data.get('first_name', ''),
+            last_name=validated_data.get('last_name', '')
+        )
+        return user
 
 
 class ReceiptSerializer(serializers.ModelSerializer):
